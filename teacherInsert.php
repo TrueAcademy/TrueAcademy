6cc6413f81e67e1e6fa663902f9ef1a4a92a3004
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-02-16 11:45:09 +0530

    added php file for teacher sign-up

---
+++ teacherInsert.php
@@ -0,0 +1,41 @@
+<?php
+
+    include("includes/dbconfig.php");
+
+    if(isset($_POST["done"])){
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $mobileno = $_POST['mobileno'];
+        $gender = $_POST['gender'];
+        $password = $_POST['password'];
+
+        $data = [
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'username' => $username,
+            'email' => $email,
+            'mobileno' => $mobileno,
+            'gender' => $gender,
+            'password' => $password,
+        ];
+
+        $collection = "teacherTable/";
+        $postdata = $database->getReference($collection)->push($data);
+
+        if($postdata){
+            echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+            header("Location : index.html"); 
+        }
+        else{
+            echo "<script type='text/javascript'>alert('failed! try again .... ')</script>";
+        }
+
+
+
+    }
+
+
+?>
