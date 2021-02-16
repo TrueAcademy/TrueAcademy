62d422dcf502aff9568f13adf69d9f75c65362f6
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-02-16 11:45:34 +0530

    added database config file

---
+++ includes/dbconfig.php
@@ -0,0 +1,19 @@
+<?php
+    require __DIR__.'/vendor/autoload.php';
+
+    use Kreait\Firebase\Factory;
+    use Kreait\Firebase\ServiceAccount;
+
+    $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/trueacademy-13962-firebase-adminsdk-cmins-96aa6aea7a.json');
+    $firebase = (new Factory())
+        ->withServiceAccount($serviceAccount)
+        ->withDatabaseUri("https://trueacademy-13962-default-rtdb.firebaseio.com")
+        ->create();
+
+    $database = $firebase -> getDatabase();
+
+    //$database = $factory->createDatabase();
+
+    // var_dump($database);
+
+?>
