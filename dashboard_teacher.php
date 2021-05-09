0500fce6291242ae76de237e26309ac4757f2250
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-05-09 11:12:07 +0530

    added to php files

---
+++ dashboard_teacher.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Dashboard.css">
+    <script src="https://use.fontawesome.com/releases/v5.15.2/js/all.js" data-auto-replace-svg="nest"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <title>Dash_board</title> 
+</head>
+<body>
+    <div class="main_div">
+        <nav>
+            <div class="left_div">
+                <a href="#" class="hamberg"><i class="fas fa-bars"></i></a>
+                <a href="#" class="logo_url"><img src="images/logo.png" alt="logo" class="logo"></a>
+                <h1 class="h1">True Academy</h1>
+            </div>
+            <div class="right_div">
+                <a href="#" class="add"><i class="fas fa-plus-circle"></i></a>
+                <a href="#" class="profile"><i class="fas fa-user"></i></a>
+            </div>
+        </nav>
+
+            <div class="sub_card">
+                <div class="center_div">
+                        <div class="card">
+                            <div class="top_div top_div_1">
+                                <h3  class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_2">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_3">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_4">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_5">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_6">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="top_div top_div_7">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="top_div top_div_8">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="top_div top_div_9">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="top_div top_div_10">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="top_div top_div_11">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="top_div top_div_12">
+                                <h3 class="h3">Rushikesh</h3>
+                                <img src="images/card_acc.jpg" alt="">
+                            </div>
+                        </div>
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+
+    </div>
+</body>
+</html>
