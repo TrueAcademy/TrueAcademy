1311da1e74ecc330d13dd1b1e3af821a1d0f5fd7
Rushi-shinde <shinderushi327@gmail.com>
2021-05-20 10:54:41 +0530

    join class done

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ var modalbtn = document.querySelector('.modal_btn_teacher');
 var modalcon = document.querySelector('.modal_teacher');
 var modalclose = document.querySelector('.close_btn_teacher');
 
-modalbtn.addEventListener('click',function() {
+modalbtn.addEventListener('click', function () {
   modalcon.classList.add('active_form')
 });
 
-modalclose.addEventListener('click',function() {
+modalclose.addEventListener('click', function () {
   modalcon.classList.remove('active_form')
 });
 /*-----------------signup page js------------------------*/
@@ -15,11 +15,11 @@ var modalbtn2 = document.querySelector('.modal_btn_student');
 var modalcon2 = document.querySelector('.modal_student');
 var modalclose2 = document.querySelector('.close_btn_student');
 
-modalbtn2.addEventListener('click',function() {
+modalbtn2.addEventListener('click', function () {
   modalcon2.classList.add('active_form')
 });
 
-modalclose2.addEventListener('click',function() {
+modalclose2.addEventListener('click', function () {
   modalcon2.classList.remove('active_form')
 });
 /*-----------------signin page teacher js------------------------*/
@@ -28,12 +28,12 @@ var modalCon3 = document.querySelector('.modal_signin_teacher');
 var modalClose3 = document.querySelector('.close_btn_signin');
 
 
-modalBtn3.addEventListener('click',function() {
-	modalCon3.classList.add('active_signin')
+modalBtn3.addEventListener('click', function () {
+  modalCon3.classList.add('active_signin')
 });
 
-modalClose3.addEventListener('click',function() {
-	modalCon3.classList.remove('active_signin')
+modalClose3.addEventListener('click', function () {
+  modalCon3.classList.remove('active_signin')
 });
 /*-----------------signin page teacher 2 for toggler span js------------------------*/
 var modalBtn3 = document.querySelector('.modal_btn_signin_teacher');
@@ -41,12 +41,12 @@ var modalCon3 = document.querySelector('.modal_signin_teacher');
 var modalClose3 = document.querySelector('.close_btn_signin');
 
 
-modalBtn3.addEventListener('click',function() {
-	modalCon3.classList.add('active_signin')
+modalBtn3.addEventListener('click', function () {
+  modalCon3.classList.add('active_signin')
 });
 
-modalClose3.addEventListener('click',function() {
-	modalCon3.classList.remove('active_signin')
+modalClose3.addEventListener('click', function () {
+  modalCon3.classList.remove('active_signin')
 });
 
 
