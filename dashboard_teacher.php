122b69f22a433003abcbcd4460ff3d513ec7cbf2
Rushi-shinde <shinderushi327@gmail.com>
2021-05-20 11:27:13 +0530

    version 1.0

--- dashboard_teacher.php
+++ dashboard_teacher.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +9,9 @@
     <script src="https://use.fontawesome.com/releases/v5.15.2/js/all.js" data-auto-replace-svg="nest"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <title>Dash_board</title> 
+    <title>Dash_board</title>
 </head>
+
 <body>
     <div class="main_div">
         <nav>
@@ -19,124 +21,147 @@
                 <h1 class="h1">True Academy</h1>
             </div>
             <div class="right_div">
-                <a href="#" class="add"><i class="fas fa-plus-circle"></i></a>
+                <a href="#" class="add" onclick="openForm()"><i class="fas fa-plus-circle"></i></a>
+                <div class="create_class_hover">Create Class</div>
                 <a href="#" class="profile"><i class="fas fa-user"></i></a>
+                <div class="profile_li">
+                    <a href="#" class="PROFILE">Profile</a>
+                    <a href="#" class="LOGOUT">Logout</a>
+                </div>
             </div>
         </nav>
 
-            <form action="createclass.php" method="post">
-                <button type="submit" name="createclass" value="createclass">create class</button>
-            </form>
-
-
-            <div class="sub_card">
-                <div class="center_div">
-
-
-                        <?php
-                            include("includes/dbconfig.php");
-
-                            $collection = "classes/";
-
-                            if(!isset($_SESSION['email'])){
-                                echo "nothing to show";
-                            }
-                            else{
-
-                            
-
-
-                        
-                        //     echo "<div class="$card">";
-                        //        echo "<div class="top_div top_div_1">"
-                        //            echo "<h3  class="h3">Rushikesh</h3>"
-                        //            echo "<img src="images/card_acc.jpg" alt="">"
-                        //         echo "</div>"
-                        //    echo " </div>"
-                        
-                        
-
-                         }   // closing isset if 
-
-                        ?>
-                        <!-- <div class="card">
-                            <div class="top_div top_div_2">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="top_div top_div_3">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="top_div top_div_4">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="top_div top_div_5">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="top_div top_div_6">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="top_div top_div_7">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="top_div top_div_8">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="top_div top_div_9">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="top_div top_div_10">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="top_div top_div_11">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="top_div top_div_12">
-                                <h3 class="h3">Rushikesh</h3>
-                                <img src="images/card_acc.jpg" alt="">
-                            </div>
-                        </div>
-                         -->
-                        
-                        
+        <div class="sub_card">
+            <div class="center_div">
+                <div class="card">
+                    <div class="top_div top_div_1">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
                     </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_2">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_3">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_4">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_5">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_6">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="top_div top_div_7">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="top_div top_div_8">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="top_div top_div_9">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="top_div top_div_10">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="top_div top_div_11">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="top_div top_div_12">
+                        <h3 class="h3">Rushikesh</h3>
+                        <img src="images/card_acc.jpg" alt="">
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+
+    </div>
+    <!-- /********************************************************************************************* -->
+    <!-- /********************************************************************************************* -->
+    <!-- /********************************************************************************************* -->
+    <!-- /********************************************************************************************* -->
+
+
+    <div class="create_class_form" id="myForm">
+        <form action="" class="mainformdiv">
+
+            <div class="close_button">
+                <a href="#" onclick="closeForm()" id="close_btn"><i id="close" class="far fa-times-circle"></i></a>
+            </div>
+
+            <div class="input_divone">
+                <div>
+                    <h4>Class Name</h4>
+                    <input class="input_CN" type="text" name="Class_Name">
                 </div>
             </div>
 
-    </div>
+
+
+            <div class="input_divtwo">
+                <div>
+                    <h4>Class Code</h4>
+                    <input class="input_CC" type="text" name="Class_Code">
+                </div>
+            </div>
+
+
+            <div class="btn_Create_class">
+                <input type="Submit" class="btn" value="Create Class" name="done">
+            </div>
+        </form>
+
+        <script>
+            function openForm() {
+                document.getElementById("myForm").style.display = "block";
+            }
+
+            function closeForm() {
+                document.getElementById("myForm").style.display = "none";
+            }
+        </script>
 </body>
+
 </html>
